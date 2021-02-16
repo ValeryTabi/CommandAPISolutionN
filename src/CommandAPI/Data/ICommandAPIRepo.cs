9b6f819512aca3db8fcd97a44f65319611b2f675
TAbiV <valldoo@yahoo.co.uk>
2021-02-16 02:22:42 +0100

    starting

---
+++ src/CommandAPI/Data/ICommandAPIRepo.cs
@@ -0,0 +1,20 @@
+using CommandAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommandAPI.Data
+{
+    public interface ICommandAPIRepo
+    {
+        bool SaveChanges();
+        
+        IEnumerable<Command> GetAllCommands();
+        Command GetCommandById(int id);
+        void CreateCommand(Command cmd);
+        void UpdateCommand(Command cmd);
+        void DeleteCommand(Command cmd);
+
+    }
+}
---
+++ src/CommandAPI/Startup.cs
@@ -0,0 +1,71 @@
+using System.Runtime.Serialization;
+using System.Transactions;
+using System.Net.Mime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using CommandAPI.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Npgsql;
+using AutoMapper;
+using Newtonsoft.Json.Serialization;
+
+
+namespace CommandAPI
+{
+    public class Startup
+    {
+        public IConfiguration Configuration {get;}
+        public Startup(IConfiguration configuration)
+        {
+          Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+
+        {
+
+            var builder = new NpgsqlConnectionStringBuilder();
+            builder.ConnectionString = Configuration.GetConnectionString("PostgreSqlConnection");
+            builder.Username = Configuration["UserID"];
+            builder.Password = Configuration["Password"];
+            services.AddDbContext<CommandContext>(opt => opt.UseNpgsql(builder.ConnectionString));
+            //SECTION 1. Add code below
+            services.AddControllers().AddNewtonsoftJson(s =>
+                            {
+                                s.SerializerSettings.ContractResolver = new
+                                CamelCasePropertyNamesContractResolver();
+                            });
+
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+             //Add the code below
+           // services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
+             services.AddScoped<ICommandAPIRepo, SqlCommandAPIRepo>();
+
+        }
+        
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseRouting();
+            
+            
+            app.UseEndpoints(endpoints =>
+            {
+                //SECTION 2. Add code below
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
